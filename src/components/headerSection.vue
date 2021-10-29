6fcc1f3dd5e767f823ca4b7da4172ef08d8084f6
chmodx5 <chmodx5@gmail.com>
2021-10-29 13:25:03 +0300

    intital commit

---
+++ src/components/headerSection.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>header section</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <header-section />
+    <form-section />
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import headerSection from "@/components/headerSection.vue";
+import formSection from "@/components/formSection.vue";
 export default {
   name: "Home",
+
   components: {
-    HelloWorld,
+    headerSection,
+    formSection,
   },
 };
 </script>
